2295e563714e4f8708536c3d2bff5455b8fb0c3d
Manuel <manuelmilillo@gmail.com>
2023-07-06 04:42:28 +0200

    Update my_component.js

--- my_component.js
+++ my_component.js
@@ -28471,28 +28471,4 @@
 //////////////////////// fin segunda
 
 
-window.portal = {};
-window.portal.pageEventListeners = {
-  onLoad: (path) => {
-    if (path === '/certificados') {
-      // Change text content of first <p> element to something
-      // else. (DOM must be loaded when onLoad is called)
-      document.getElementsByTagName('h2')[0].textContent =
-          'Texto modificadodesde script';
-      // print a custom message to the console every second while user is on
-      // quickstart page.
-      const interval =
-          window.setInterval(() => console.log('Hello'), 1000);
-      return interval;
-    }
-    return undefined;
-  },
-  onUnload: (path, contextReturnedFromOnLoad) => {
-    if (contextReturnedFromOnLoad != null) {
-      // Stop printing custom message to console every second.
-      window.clearInterval(contextReturnedFromOnLoad)
-
-    }
-  },
-};
 
